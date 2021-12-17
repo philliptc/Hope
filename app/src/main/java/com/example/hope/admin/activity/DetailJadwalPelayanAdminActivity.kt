e7aa9653262b86d69e812d1bf30b71281f6ca3c7
philliptc <phill.tc05@gmail.com>
2021-12-17 20:48:47 +0700

    layout 80%

---
+++ app/src/main/java/com/example/hope/admin/activity/DetailJadwalPelayanAdminActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hope.admin.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hope.R
+
+class DetailJadwalPelayanAdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_jadwal_pelayan_admin)
+    }
+}
---
+++ app/src/main/java/com/example/hope/admin/activity/ProfileJemaatAdminActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hope.admin.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hope.R
+
+class ProfileJemaatAdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_jemaat_admin)
+    }
+}
